06ce3cde6c03090f8d417426128ace1dd850b42a
Reto Kaufmann <reto.kaufmann@adon.li>
2016-09-28 21:51:41 +0200

    neus package

---
+++ Semester2/src/Fach_98_Test/TestKlasse.java
@@ -0,0 +1,10 @@
+package Fach_98_Test;
+
+/**
+ * Created by rk on 28.09.16.
+ */
+public class TestKlasse {
+    public static void main(String[] args) {
+        System.out.println("hallo");
+    }
+}
--- Semester2/src/Fach_99_Test/TestKlasse2.java
+++ Semester2/src/Fach_99_Test/TestKlasse2.java
@@ -3,7 +3,7 @@ package Fach_99_Test;
 /**
  * Created by rk on 28.09.16.
  */
-public class TestKlasse {
+public class TestKlasse2 {
     public static void main(String[] args) {
         System.out.println("hallo");
     }
