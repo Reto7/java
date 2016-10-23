1e97274705286bc254dbb34e82de60d9313e6d9c
Reto Kaufmann <reto.kaufmann@adon.li>
2016-10-23 14:32:00 +0200

    Interfaces anhand des Delegate Design Patterns (2)

---
+++ Semester1/src/Fach_2_Objektorientierung/A90_Interfaces_Delegate/ColorPrinter.java
@@ -0,0 +1,17 @@
+package Fach_2_Objektorientierung.A90_Interfaces_Delegate;
+
+/**
+ * http://www.programmierenlernenhq.de/einfuehrung-in-java-design-patterns-software-entwurfsmuster/
+ * -----------------------------------------------------------------------------
+ * Die ganze Loesung wird so erweiterbar. Neue Printer muessen einfach nur das
+ * Printable Interface implementieren!
+ * Es muss weder Printer noch Printable angepasst werden
+ * -----------------------------------------------------------------------------
+ */
+public class ColorPrinter implements Printable // Delegate
+{
+    public void print()
+    {
+        System.out.println("ColorPrinter prints!");
+    }
+}
