e23279695ef0be53b149e1aadad64d8ae10d5386
Reto Kaufmann <reto.kaufmann@adon.li>
2016-10-23 20:04:15 +0200

    Abstract anhand des TemplateMethod Design Patterns (2)

--- Semester1/src/Fach_2_Objektorientierung/A90_Abstract_TemplateMethod_Pattern/KoffeinhaltigesGetraenk.java
+++ Semester1/src/Fach_2_Objektorientierung/A90_Abstract_TemplateMethod_Pattern/KoffeinhaltigesGetraenk.java
@@ -1,7 +1,8 @@
 package Fach_2_Objektorientierung.A90_Abstract_TemplateMethod_Pattern;
 
 /**
- * Created by user on 23.10.2016.
+ * HEAD FIRST DESIGN PATTERNS
+ * https://books.google.li/books?id=k4_7uAJrwkEC&pg=PA305&lpg=PA305&dq=java+template+method+pattern&source=bl&ots=lF-ZrvkdTV&sig=HPNXNTyeUcRVazuxMeXzC8cQIRg&hl=de&sa=X&ved=0ahUKEwj91fnd_PDPAhVMDxoKHVLFATE4ChDoAQgvMAQ#v=onepage&q=java%20template%20method%20pattern&f=false
  */
 public abstract class KoffeinhaltigesGetraenk {
 
