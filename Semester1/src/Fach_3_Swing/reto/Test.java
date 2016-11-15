5bd881a5c1bbffb53a80af0e97bac5dfbd0f41a8
Reto Kaufmann <reto.kaufmann@adon.li>
2016-11-15 20:58:38 +0100

    von michele

---
+++ Semester1/src/Fach_3_Swing/reto/Test.java
@@ -0,0 +1,14 @@
+package Fach_3_Swing.reto;
+
+import javax.swing.*;
+
+/**
+ * Created by rk on 15.11.16.
+ */
+public class Test {
+    private JButton halloButton;
+    private JPanel panel1;
+    private JButton button1;
+
+
+}
---
+++ Semester1/src/Fach_3_Swing/reto/TestRahmen.java
@@ -0,0 +1,10 @@
+package Fach_3_Swing.reto;
+
+/**
+ * Created by rk on 15.11.16.
+ */
+public class TestRahmen {
+    public static void main(String[] args) {
+        Test t = new Test();
+    }
+}
