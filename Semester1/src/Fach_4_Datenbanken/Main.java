4d829b840fa8ac4cf0171f5b9494a8357f65edf1
Reto Kaufmann <reto.kaufmann@adon.li>
2016-12-06 18:55:42 +0100

    jdbc start 2

--- Semester1/src/Fach_4_Datenbanken/Main.java
+++ Semester1/src/Fach_4_Datenbanken/Main.java
@@ -1,7 +1,6 @@
 package Fach_4_Datenbanken;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 /**
  * Created by rk on 06.12.16.
@@ -13,12 +12,27 @@ public class Main {
         Class.forName("org.postgresql.Driver");
 
         //DB-Verbindung aufbauen:
-        Connection v_conn = DriverManager.getConnection(
-                "jdbc:postgresql://localhost/byceco",
-                "postgres","postgres");
+        //Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/byceco", "postgres","postgres");
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/reto", "postgres","postgres");
 
-
-
-        System.out.println("Hello World");
+        // Statische Abfrage (Statement) erstellen
+        Statement statement = connection.createStatement();
+        // CreateTable -Anweisung ausführen
+        statement.execute(" DROP TABLE if exists laender");
+        statement.execute(" CREATE TABLE laender (land_name varchar(100))");
+        // Insert-Anweisung ausführen
+        statement.executeUpdate(" INSERT INTO laender VALUES('Deutschland')");
+        statement.executeUpdate(" INSERT INTO laender VALUES('Schweiz')");
+        // Select ausführen
+        ResultSet resultset = statement.executeQuery("SELECT land_name FROM laender");
+        // Resultat!
+        // wie ein Cursor mit Zeiger. Steht Anfangs immer auf 1. Zeile. Falls next() false liefert, ist Zeiger am Schluss angekommen.
+        while (resultset.next()){
+            System.out.println(resultset.getString("land_name"));
+        }
+        // Statement und Verbindung schließen
+        statement.close();
+        connection.close();
+        System.out.println("...fertig");
     }
 }
