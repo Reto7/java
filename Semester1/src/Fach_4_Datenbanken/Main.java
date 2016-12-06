b778b89148f283eb65cde742d7ed7e7a31daee95
Reto Kaufmann <reto.kaufmann@adon.li>
2016-12-06 19:03:24 +0100

    jdbc start 3

--- Semester1/src/Fach_4_Datenbanken/Main.java
+++ Semester1/src/Fach_4_Datenbanken/Main.java
@@ -19,20 +19,30 @@ public class Main {
         Statement statement = connection.createStatement();
         // CreateTable -Anweisung ausführen
         statement.execute(" DROP TABLE if exists laender");
-        statement.execute(" CREATE TABLE laender (land_name varchar(100))");
+        statement.execute(" CREATE TABLE laender (land_kuerzel varchar(2) PRIMARY KEY, land_name varchar(100))");
         // Insert-Anweisung ausführen
-        statement.executeUpdate(" INSERT INTO laender VALUES('Deutschland')");
-        statement.executeUpdate(" INSERT INTO laender VALUES('Schweiz')");
+        statement.executeUpdate(" INSERT INTO laender (land_kuerzel, land_name) VALUES('DE','Deutschland')");
+        statement.executeUpdate(" INSERT INTO laender (land_kuerzel, land_name) VALUES('CH','Schweiz')");
         // Select ausführen
-        ResultSet resultset = statement.executeQuery("SELECT land_name FROM laender");
+        ResultSet resultset = statement.executeQuery("SELECT land_name FROM laender where land_kuerzel = 'CH' ");
         // Resultat!
         // wie ein Cursor mit Zeiger. Steht Anfangs immer auf 1. Zeile. Falls next() false liefert, ist Zeiger am Schluss angekommen.
         while (resultset.next()){
-            System.out.println(resultset.getString("land_name"));
+            System.out.println("Row: " +resultset.getString("land_name"));
         }
-        // Statement und Verbindung schließen
+        System.out.println("----------------------------------------");
+        // Select ausführen
+        resultset = statement.executeQuery("SELECT count(*) as anzahl FROM laender");
+        // Resultat!
+        // wie ein Cursor mit Zeiger. Steht Anfangs immer auf 1. Zeile. Falls next() false liefert, ist Zeiger am Schluss angekommen.
+        while (resultset.next()){
+            System.out.println("Anzahl: " +resultset.getString("anzahl"));
+        }
+        System.out.println("----------------------------------------");
+        // alles wieder sauber schliessen
+        resultset.close();
         statement.close();
         connection.close();
-        System.out.println("...fertig");
+
     }
 }
