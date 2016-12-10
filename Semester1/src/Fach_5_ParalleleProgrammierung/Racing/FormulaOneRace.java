faae07bf9d06a2ed1234840f10cb677f3b5b04d6
Reto Kaufmann <reto.kaufmann@adon.li>
2016-12-10 21:44:30 +0100

    racecar beispiel mit threads, NEUE racing objekte ohne 2D grafiken

--- Semester1/src/Fach_5_ParalleleProgrammierung/Racing/FormulaOneRace.java
+++ Semester1/src/Fach_5_ParalleleProgrammierung/Racing/FormulaOneRace.java
@@ -1,12 +1,18 @@
 package Fach_5_ParalleleProgrammierung.Racing;
 
+import Fach_3_Swing.ch.ibw.swing.events.Farbwechsel;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by user on 10.12.2016.
  */
 public class FormulaOneRace {
+
+    private static final int anzahlRunden = 50;
 
     /**
      * Create the GUI and show it.  For thread safety,
@@ -22,7 +28,12 @@ public class FormulaOneRace {
         frame.setLocationRelativeTo(null);
 
         // single column grid layout
-        frame.setLayout(new GridLayout(3,1));
+        frame.setLayout(new GridLayout(6,1));
+
+        JLabel labelTotalAnzeige = new JLabel("Total KM aller Fahrer (" +anzahlRunden +" Runden)");
+        frame.getContentPane().add(labelTotalAnzeige);
+        JLabel labelTotalKM = new JLabel();
+        frame.getContentPane().add(labelTotalKM);
 
         JLabel labelCar1 = new JLabel("Car 1");
         frame.getContentPane().add(labelCar1);
@@ -33,19 +44,55 @@ public class FormulaOneRace {
         JLabel labelCar3 = new JLabel("Car 3");
         frame.getContentPane().add(labelCar3);
 
+        //////////////////////////////////////////
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(2,2));
+        frame.getContentPane().add(buttonPanel);
+
+        JButton stop1 = new JButton("stop 1");
+        stop1.setPreferredSize(new Dimension(20,20));
+        buttonPanel.add(stop1);
+        JButton stop2 = new JButton("stop 2");
+        stop2.setPreferredSize(new Dimension(20,20));
+        buttonPanel.add(stop2);
+        JButton stop3 = new JButton("stop 3");
+        stop3.setPreferredSize(new Dimension(20,20));
+        buttonPanel.add(stop3);
+        JButton start1 = new JButton("start 1");
+        start1.setPreferredSize(new Dimension(20,20));
+        buttonPanel.add(start1);
+        JButton start2 = new JButton("start 2");
+        start2.setPreferredSize(new Dimension(20,20));
+        buttonPanel.add(start2);
+        JButton start3 = new JButton("start 3");
+        start3.setPreferredSize(new Dimension(20,20));
+        buttonPanel.add(start3);
+
+        // Listener-Objekt erzeugen und beim Button registrieren
+        ButtonListener bL = new ButtonListener();
+        stop1.addActionListener(bL);
+
         //Display the window.
         frame.pack();
         frame.setVisible(true);
 
         // START RACE
-
-        RaceCar car1 = new RaceCar("Hamilton", labelCar1);
+        RaceCar car1 = new RaceCar("Hamilton", labelCar1, anzahlRunden, labelTotalKM);
         car1.start();
 
-        RaceCar car2 = new RaceCar("Kaufmann", labelCar2);
+        RaceCar car2 = new RaceCar("Kaufmann", labelCar2, anzahlRunden, labelTotalKM);
         car2.start();
 
+        RaceCar car3 = new RaceCar("Rosberg", labelCar3, anzahlRunden, labelTotalKM);
+        car3.start();
     }
+
+    // Innere Button-Listener-Klasse
+    private static class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        }
+    }
+
 
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
@@ -57,3 +104,4 @@ public class FormulaOneRace {
         });
     }
 }
+
--- Semester1/src/Fach_5_ParalleleProgrammierung/Racing/RaceCar.java
+++ Semester1/src/Fach_5_ParalleleProgrammierung/Racing/RaceCar.java
@@ -1,5 +1,6 @@
 package Fach_5_ParalleleProgrammierung.Racing;
 
+import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.*;
 
 /**
@@ -8,12 +9,20 @@ import javax.swing.*;
 public class RaceCar extends Thread {
 
     private JLabel labelCar;
+    private JLabel labelTotalKM;
     private String driverName;
+    private int anzahlRunden;
+    private String lock = "lock";
+
+    // zentrale variable um die anzahl km aller fahrer mitzuzaehlen
+    private static int totalKmAllerFahrer;
 
     // constructor
-    public RaceCar(String driverName, JLabel labelCar) {
+    public RaceCar(String driverName, JLabel labelCar, int anzahlRunden, JLabel labelTotalKM) {
         this.driverName = driverName;
         this.labelCar = labelCar;
+        this.anzahlRunden = anzahlRunden;
+        this.labelTotalKM = labelTotalKM;
     }
 
     // das in der run() Methode kann parallel ablaufen
@@ -21,25 +30,35 @@ public class RaceCar extends Thread {
     public void run() {
         System.out.println("Started " +driverName);
 
-        int nextPosition;
-        for (int i=1; i<=10; i++) {
-
+        int kmGefahren;
+        for (int i=1; i<=this.anzahlRunden; i++) {
             String out = "";
-            nextPosition = 1+(i*2);
-            System.out.println(driverName +" " +i +": " +nextPosition);
-            for (int x=1; x<nextPosition; x++) {
+            kmGefahren = 1+(i*2);
+            System.out.println(driverName +" " +i +": " +kmGefahren);
+            for (int x=1; x<kmGefahren; x++) {
                 out = out+"*";
             }
-            System.out.println(out);
+            out = out + kmGefahren +"km";
+            //System.out.println(out);
             labelCar.setText(out);
+            // gefahrene KM in total hochzaehlen
+            totalKmAllerFahrer = totalKmAllerFahrer + kmGefahren;
+            labelTotalKM.setText(String.valueOf(totalKmAllerFahrer));
+
 
             try {
-                Thread.sleep(200);
+                Thread.sleep(getRandomTimeForSleeper(1,400));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
+        System.out.println("totalKmAllerFahrer " +totalKmAllerFahrer);
+    }
+
+    private int getRandomTimeForSleeper(int min, int max) {
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
 }
