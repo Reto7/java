c486fc3ee8fc53f2cc9987698c2afeef3479e70c
Reto Kaufmann <reto.kaufmann@adon.li>
2016-12-11 15:35:37 +0100

    thread beispiele

--- Semester1/src/Fach_5_ParalleleProgrammierung/Basis/Counter.java
+++ Semester1/src/Fach_5_ParalleleProgrammierung/Basis/Counter.java
@@ -5,11 +5,17 @@ package Fach_5_ParalleleProgrammierung.Basis;
  */
 public class Counter implements Runnable{
 
+    String counterName;
+
+    public Counter(String counterName) {
+        this.counterName = counterName;
+    }
+
     // das in der run() Methode kann parallel ablaufen
     @Override
     public void run() {
         for (int i=1;  i<=10; i++) {
-            System.out.println("Zaehler: " +i);
+            System.out.println(counterName +", Zaehler: " +i);
             try {
                 // 1 Sekunde warten
                 Thread.sleep(1000);
--- Semester1/src/Fach_5_ParalleleProgrammierung/Basis/CounterTest.java
+++ Semester1/src/Fach_5_ParalleleProgrammierung/Basis/CounterTest.java
@@ -7,9 +7,9 @@ package Fach_5_ParalleleProgrammierung.Basis;
 public class CounterTest {
 
     public static void main(String[] args) {
-        Thread t = new Thread(new Counter());
+        Thread t = new Thread(new Counter("counter1"));
         t.start();
-        Thread t2 = new Thread(new Counter());
+        Thread t2 = new Thread(new Counter("counter2"));
         t2.start();
     }
 
