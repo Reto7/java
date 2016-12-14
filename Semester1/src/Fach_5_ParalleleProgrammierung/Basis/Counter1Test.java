74d0dc04d39f4e4f0c2a6afe284f4f6301be14d6
Reto Kaufmann <reto.kaufmann@adon.li>
2016-12-14 20:53:33 +0100

    renamed

--- Semester1/src/Fach_5_ParalleleProgrammierung/Basis/Counter1Test.java
+++ Semester1/src/Fach_5_ParalleleProgrammierung/Basis/Counter1Test.java
@@ -4,12 +4,12 @@ package Fach_5_ParalleleProgrammierung.Basis;
 /**
  * Created by rk on 08.12.16.
  */
-public class CounterTest {
+public class Counter1Test{
 
     public static void main(String[] args) {
-        Thread t = new Thread(new Counter("counter1"));
+        Thread t = new Thread(new Counter1("counter1"));
         t.start();
-        Thread t2 = new Thread(new Counter("counter2"));
+        Thread t2 = new Thread(new Counter1("counter2"));
         t2.start();
     }
 
