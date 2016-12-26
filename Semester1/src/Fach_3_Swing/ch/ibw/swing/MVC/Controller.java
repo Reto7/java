9e1e9b6131bb8257e43c1271c89017febbdc6e0b
Reto Kaufmann <reto.kaufmann@adon.li>
2016-12-26 22:13:21 +0100

    einfaches Swing MVC Beispiel

---
+++ Semester1/src/Fach_3_Swing/ch/ibw/swing/MVC/Controller.java
@@ -0,0 +1,44 @@
+package Fach_3_Swing.ch.ibw.swing.MVC;
+
+/**
+ * Created by user on 26.12.2016.
+ */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller {
+
+    /**
+     * The Controller class keeps references to Model and View classes.
+     * So Controller sees Model and View interfaces. Controller provides
+     * interaction between Model and View. Method control() gets reference
+     * to view's button and assign actionListener to it. In actionListener's
+     * method actionPerformed() method linkBtnAndLabel() is called.
+     * In linkBtnAndLabel() model's variable "x" increments and than "x"
+     * sends to view's label to display changes.
+     */
+
+    private Model model;
+    private View view;
+    private ActionListener actionListener;
+
+    /* constructor */
+    public Controller(Model model, View view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void control(){
+        actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                linkBtnAndLabel();
+            }
+        };
+        view.getButton().addActionListener(actionListener);
+    }
+
+    private void linkBtnAndLabel(){
+        model.incX();
+        view.setText(Integer.toString(model.getX()));
+    }
+}
