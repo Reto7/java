8b158dc49ad26709a9c9e29b01b9d69faf6a074a
Reto Kaufmann <reto.kaufmann@adon.li>
2017-09-19 20:26:14 +0200

    selenium stand schule

--- Semester1/src/main/java/TestautomatisierungKeywords/Keywords.java
+++ Semester1/src/main/java/TestautomatisierungKeywords/Keywords.java
@@ -6,8 +6,19 @@ import com.borland.silk.keyworddriven.annotations.Keyword;
 public class Keywords {
 
 
-  @Keyword("Wer ist da")
-  public void myKeyword(@Argument("Benutzername") String name) {
-    System.out.println(name);
+  // dies hier dient alles dazu, dass auf der SILK TESTSUITE dann der Automatisierer (Person) sich nicht mit technischen
+  // Begriffen wie dem Methodennamen "myKeyword" oder Parameternamen "userNameX" rumschlagen muss.
+  // In der Silk Testsuite verwendet er dann "Login auf Webseite" und "Benutzername"
+  // Instanziert wird diese Klasse durch die SILK TESTSUITE
+  // beispiele keywords:
+  // - öffne webseite V20
+  // - öffne dialog
+  // - erstelle parkbewilligung
+  // - überprüfe pdf beleg
+  @Keyword("Login auf Webseite")
+  public void myKeyword(@Argument("Benutzername") String userNameX) {
+
+    System.out.println(userNameX); // dies hier ist dann der effektive Test, z.B. Selenium. Hier im Beispiel einfach SOUT
+
   }
 }
