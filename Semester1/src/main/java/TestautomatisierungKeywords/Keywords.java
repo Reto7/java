21521290b0e4ecb9a36031e0846394f6cb759fab
Reto Kaufmann <reto.kaufmann@adon.li>
2017-09-19 20:22:31 +0200

    Revert "Keywords"
    This reverts commit 78b0921e7e6bf6a6671cea6ffb581e8a190f758f.

--- Semester1/src/main/java/TestautomatisierungKeywords/Keywords.java
+++ Semester1/src/main/java/TestautomatisierungKeywords/Keywords.java
@@ -6,19 +6,8 @@ import com.borland.silk.keyworddriven.annotations.Keyword;
 public class Keywords {
 
 
-  // dies hier dient alles dazu, dass auf der SILK TESTSUITE dann der Automatisierer (Person) sich nicht mit technischen
-  // Begriffen wie dem Methodennamen "myKeyword" oder Parameternamen "userNameX" rumschlagen muss.
-  // In der Silk Testsuite verwendet er dann "Login auf Webseite" und "Benutzername"
-  // Instanziert wird diese Klasse durch die SILK TESTSUITE
-  // beispiele keywords:
-  // - öffne webseite V20
-  // - öffne dialog
-  // - erstelle parkbewilligung
-  // - überprüfe pdf beleg
-  @Keyword("Login auf Webseite")
-  public void myKeyword(@Argument("Benutzername") String userNameX) {
-
-    System.out.println(userNameX); // dies hier ist dann der effektive Test, z.B. Selenium. Hier im Beispiel einfach SOUT
-
+  @Keyword("Wer ist da")
+  public void myKeyword(@Argument("Benutzername") String name) {
+    System.out.println(name);
   }
 }
